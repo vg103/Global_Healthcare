336bfefd3885a034f7e53ea61d2e0cf71d132ca1
Jay Sanghavi <jay.sanghavi@outlook.com>
2025-02-27 20:34:54 -0800

    done styling for some files

--- data_prep/data_prep.py
+++ data_prep/data_prep.py
@@ -1,6 +1,19 @@
+"""
+This module contains functions for data preparation.
+"""
+
 import pandas as pd
 
-def import_data(data_path=""):
-    df = pd.read_csv(data_path)
 
+def import_data(data_path=""):
+    """
+    Import data from a CSV file.
+
+    Parameters:
+    data_path (str): The path to the CSV file.
+
+    Returns:
+    DataFrame: A pandas DataFrame containing the imported data.
+    """
+    df = pd.read_csv(data_path)
     return df
--- tests/test_data_prep.py
+++ tests/test_data_prep.py
@@ -1,17 +1,20 @@
+"""
+Unit tests for the data preparation module.
+"""
+
 import unittest
-import numpy as np
 from data_prep.data_prep import import_data
 
+
 class TestDataPrep(unittest.TestCase):
+    """Test cases for the data preparation functions."""
+
     def test_df_exists(self):
+        """Test that the DataFrame is not empty after importing data."""
         df = import_data(data_path="data/scraped_doc.csv")
         num_rows = df.shape[0]
         self.assertGreater(num_rows, 0, "data is empty")
 
-        
+
 if __name__ == '__main__':
     unittest.main()
-        
-        
-# suite = unittest.TestLoader().loadTestsFromTestCase(UnitTests)
-# _ = unittest.TextTestRunner().run(suite)
